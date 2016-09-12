7653eca909dbe42246a8b37ea56b8ee8035cd1e3
Andy <andrew.sunglae.kim@gmail.com>
2016-09-12 15:01:14 -0400

    updatesss

--- updateCohort.js
+++ updateCohort.js
@@ -4,6 +4,8 @@ var prompt = require("prompt")
 
 console.log("What is the new cohort number?")
 prompt.start()
-prompt.get(['cohortNum'], function (err, result) {
+prompt.get(['cohortNum'], function (err, result) {t
   console.log('  Cohort Number: ' + result.cohortNum);
 });
+
+cohorter.newCohort(result.cohortNum)
