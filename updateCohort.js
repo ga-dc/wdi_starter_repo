b2b993269ac129a2330cc69e244d6def3d614e9f
Andy <andrew.sunglae.kim@gmail.com>
2016-09-12 15:01:41 -0400

    mov ecohorter into callback

--- updateCohort.js
+++ updateCohort.js
@@ -6,6 +6,5 @@ console.log("What is the new cohort number?")
 prompt.start()
 prompt.get(['cohortNum'], function (err, result) {t
   console.log('  Cohort Number: ' + result.cohortNum);
+  cohorter.newCohort(result.cohortNum)
 });
-
-cohorter.newCohort(result.cohortNum)
